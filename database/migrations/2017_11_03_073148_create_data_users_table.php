0841957635aeddc06dcde477b1f71f884f571798
aditrenaldi8 <aditrenaldi8@gmail.com>
2017-11-03 14:49:41 +0700

    Update Authentication

---
+++ database/migrations/2017_11_03_073148_create_data_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('bio');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('data_users');
+    }
+}
